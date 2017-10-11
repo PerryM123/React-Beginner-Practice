f14b0a414093159bd3393cacacfb2e32202d9cd7
PerryM123 <n/a>
2017-10-11 18:43:15 +0900

    Added state logic.
    Had a lot of trouble with this one. When using functions, they must be binded(???). Maybe this is one-way binding occurring???

--- js/my-code.js
+++ js/my-code.js
@@ -64,13 +64,20 @@ class AboutUsComponent extends React.Component {
 class IncreaseValueComponent extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      value: 0
+    };
+    this.change = this.change.bind(this);
+  }
+  change(e) {
+    this.setState({ value: this.state.value + 1 });
   }
   render() {
     return (
       <div>
-        <span>VALUE HERE</span>
+        <span>{this.state.value}</span>
         <div>
-          <input type="button" value="+" />
+          <input type="button" value="+" onClick={this.change} />
         </div>
       </div>
     )
